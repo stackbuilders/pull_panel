80db5734b94e9f0a51c5081d8a0a8ffd608c2487
Justin S. Leitgeb <justin@stackbuilders.com>
2012-02-19 08:12:45 -0500

    Initial commit

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject pull-panel "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.1"]
+                           [clj-http "0.2.7"]]
             :main pull-panel.server)
 
